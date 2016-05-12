fa92cade1c1bf88c10e065a71c32a2d0563cc492
Giovanne Afonso <giovanneafonso@gmail.com>
2016-05-12 16:00:31 -0400

    Add shortcodes functions to the plugin loader
    Now I can add multiple shortcuts everytime I want to

--- includes/class-progress-timeline-loader.php
+++ includes/class-progress-timeline-loader.php
@@ -40,6 +40,15 @@ class Progress_Timeline_Loader {
 	 * @var      array    $filters    The filters registered with WordPress to fire when the plugin loads.
 	 */
 	protected $filters;
+    
+	/**
+	 * The array of shortcodes registered with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      array    $shortcodes    The shortcodes registered with WordPress to fire when the plugin loads.
+	 */
+	protected $shortcodes;
 
 	/**
 	 * Initialize the collections used to maintain the actions and filters.
@@ -80,6 +89,18 @@ class Progress_Timeline_Loader {
 	public function add_filter( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
 		$this->filters = $this->add( $this->filters, $hook, $component, $callback, $priority, $accepted_args );
 	}
+    
+    /**
+     * Add a new shortcode to the collection to be registered with WordPress
+     *
+     * @since     1.0.0
+     * @param     string        $tag           The name of the new shortcode.
+     * @param     object        $component      A reference to the instance of the object on which the shortcode is defined.
+     * @param     string        $callback       The name of the function that defines the shortcode.
+     */
+    public function add_shortcode( $tag, $component, $callback) {
+        $this->shortcodes = $this->add( $this->shortcodes, $tag, $component, $callback, null, null );
+    }
 
 	/**
 	 * A utility function that is used to register the actions and hooks into a single
@@ -123,6 +144,10 @@ class Progress_Timeline_Loader {
 		foreach ( $this->actions as $hook ) {
 			add_action( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
 		}
+
+        foreach ( $this->shortcodes as $hook ) {
+            add_shortcode( $hook['hook'], array( $hook['component'], $hook['callback'] ) );
+        }
 
 	}
 
